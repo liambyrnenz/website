138cc67094907c2baebbf89adf69fcbe7b01a65f
Liam <liam.jb1@gmail.com>
2018-01-17 20:56:47 +1300

    Text reuse via JS and reformatting

--- scripts/text.js
+++ scripts/text.js
@@ -1,3 +1,10 @@
-function pageHead() {
-    document.getElementById(null).innerHTML = null
+var TITLE = "Liam Byrne | github.io";
+var FOOTER_TEXT = "Footer text!";
+
+function setPageTitle() {
+    document.title = TITLE;
+}
+
+function pageFooter() {
+    document.getElementById("footer").innerHTML = FOOTER_TEXT;
 }
