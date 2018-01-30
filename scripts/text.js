c7ad620936877775404028906c7c69a3182cbc4a
Liam <liam.jb1@gmail.com>
2018-01-30 18:25:56 +1300

    New text

--- scripts/text.js
+++ scripts/text.js
@@ -4,15 +4,10 @@
  */
 
 var TAB_TEXT = " | Liam Byrne";
-var SUBTITLE = "PROSPECTIVE BE(HONS)";
 var FOOTER_TEXT = "Created by Liam Byrne on GitHub Pages";
 
 function pageTitle() {
     document.getElementById("tab").innerHTML += TAB_TEXT;
-}
-
-function subtitle() {
-    document.getElementById("sub").innerHTML = SUBTITLE;
 }
 
 function pageFooter() {
