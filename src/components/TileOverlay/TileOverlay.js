fc248c226ec66eb837e4619e8fe17afe9757d6f5
Liam <liam.jb1@gmail.com>
2019-03-10 12:22:35 +1300

    Dynamic images in tiles

--- src/components/TileOverlay/TileOverlay.js
+++ src/components/TileOverlay/TileOverlay.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import './TileOverlay.css';
 
+// TODO: Import all images here
+import Image from "../../assets/example.png"
+
 class TileOverlay extends Component {
 
   render() {
     return (
-      <div className="cover">
+      <div className="cover" style={{backgroundImage: "url(" + Image + ")"}}>
         {this.props.children}
       </div>
     );
