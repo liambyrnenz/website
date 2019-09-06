5facdb3fdc14af744a09c21360f84ae3ca84c571
Liam <liam.jb1@gmail.com>
2019-09-06 20:31:00 +1200

    Add initial styling to test page

--- src/pages/PageTest/PageTest.js
+++ src/pages/PageTest/PageTest.js
@@ -9,7 +9,19 @@ class PageTest extends Component {
   render() {
     return (
       <div>
-        Hi!
+        <p class="title">Title</p>
+        <p class="content">Tempore atque optio enim. Aliquam provident amet hic id et. Quisquam blanditiis ullam expedita.
+        Alias consectetur dignissimos quos est velit sint. Consequatur temporibus voluptatum consequatur
+        delectus et qui. Explicabo praesentium quidem consequuntur ipsa voluptatem.</p>
+
+        <p class="content">Tempore atque optio enim. Aliquam provident amet hic id et. Quisquam blanditiis ullam expedita.
+        Alias consectetur dignissimos quos est velit sint. Consequatur temporibus voluptatum consequatur
+        delectus et qui. Explicabo praesentium quidem consequuntur ipsa voluptatem.</p>
+
+        <p class="content">Tempore atque optio enim. Aliquam provident amet hic id et. Quisquam blanditiis ullam expedita.
+        Alias consectetur dignissimos quos est velit sint. Consequatur temporibus voluptatum consequatur
+        delectus et qui. Explicabo praesentium quidem consequuntur ipsa voluptatem.
+        </p>
       </div>
     );
   }
