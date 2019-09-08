b95aed9aa4b32d115a3ebb2b32b084886c6b7b51
Liam <liam.jb1@gmail.com>
2019-09-08 17:42:03 +1200

    Utiltise transitions for header button animations

--- src/components/HeaderButton/HeaderButton.js
+++ src/components/HeaderButton/HeaderButton.js
@@ -3,24 +3,36 @@ import './HeaderButton.css';
 
 class HeaderButton extends Component {
 
-  render() {
+  getContainerClassName() {
     if (this.props.menu) {
-      return (
-        <div className="hamburger-container" onClick={this.props.onButtonClicked}>
-          <div className="hamburger-bar" id="hamburger-bar-1"></div>
-          <div className="hamburger-bar" id="hamburger-bar-2"></div>
-          <div className="hamburger-bar" id="hamburger-bar-3"></div>
-        </div>
-      );
+      return "hamburger-container";
     } else if (this.props.close) {
-      return (
-        <div className="hamburger-container hamburger-container-padded" onClick={this.props.onButtonClicked}>
-          <div className="hamburger-cross-1"></div>
-          <div className="hamburger-cross-2"></div>
-        </div>
-      )
+      return "hamburger-container hamburger-container-padded";
     }
   }
+
+  getBarClassName(id) {
+    if (this.props.menu) {
+      return "hamburger-bar";
+    } else if (this.props.close) {
+      if (id === 1) {
+        return "hamburger-cross-1";
+      } else if (id === 3) {
+        return "hamburger-cross-2";
+      }
+    }
+  }
+
+  render() {
+    return (
+      <div className={this.getContainerClassName()} onClick={this.props.onButtonClicked}>
+        <div className={this.getBarClassName(1)}></div>
+        <div className={this.getBarClassName(2)}></div>
+        <div className={this.getBarClassName(3)}></div>
+      </div>
+    );
+  }
+
 }
 
 export default HeaderButton;
