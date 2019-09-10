823756d35240706be18496a7e21b044df8721e89
Liam <liam.jb1@gmail.com>
2019-09-10 20:18:22 +1200

    Make logo act as link to home page

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
 import './Logo.css';
 
 class Logo extends Component {
 
   render() {
     return (
-      <div className={"text-container " + (this.props.hidden ? "invisible" : "visible")}>
-        <p className="heading">Liam Byrne</p>
-        <p className="subheading">JUNIOR SOFTWARE ENGINEER</p>
-      </div>
+      <Link to="/" className="logo-link">
+        <div className={"text-container " + (this.props.hidden ? "invisible" : "visible")}>
+          <p className="heading">Liam Byrne</p>
+          <p className="subheading">JUNIOR SOFTWARE ENGINEER</p>
+        </div>
+      </Link>
     );
   }
 
