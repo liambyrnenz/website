fae239ed46b04d8ca2fd05ec8ec257663de0f142
Liam <liam.jb1@gmail.com>
2019-09-12 19:37:04 +1200

    Fix sizing in content photo text component

--- src/components/ContentPhotoText/ContentPhotoText.js
+++ src/components/ContentPhotoText/ContentPhotoText.js
@@ -5,12 +5,20 @@ import './ContentPhotoText.css';
 class ContentPhotoText extends Component {
 
   render() {
+    let image = require("../../assets/me.png")
     return (
       <div className="content-phototext-container">
-        <div className="content-phototext-photo">
+        <div className="content-phototext-photo" style={
+          {
+            backgroundImage: `url(${image})`,
+            backgroundPosition: "center",
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "cover"
+          }
+        }>
         </div>
         <div className="content-phototext-text">
-        Text
+          Text
         </div>
       </div>
     );
