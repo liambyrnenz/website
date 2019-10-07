a340156505581f04749bfc98c0c4a9cf8ea5b8c2
Liam <liam.jb1@gmail.com>
2019-10-08 12:04:16 +1300

    Version 1.1, build 15

---
+++ precache-manifest.fe87b42d8b8639ea7f0506fe16b23c8c.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9262ce5f6fcc6a515b3ee6037ac207ce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d953ad2b046bb0bd5864",
+    "url": "/static/css/main.7c45ce83.chunk.css"
+  },
+  {
+    "revision": "fcc01a70f5558f0d8490",
+    "url": "/static/js/2.7ee1c7cc.chunk.js"
+  },
+  {
+    "revision": "d953ad2b046bb0bd5864",
+    "url": "/static/js/main.f1ad7a55.chunk.js"
+  },
+  {
+    "revision": "994dac7166d0f20d533d",
+    "url": "/static/js/runtime~main.b259a76e.js"
+  },
+  {
+    "revision": "8d08e6aca167d23d834ae38fac27a111",
+    "url": "/static/media/alphero.8d08e6ac.png"
+  },
+  {
+    "revision": "0838124fb0bd39177774d4eed8ffb666",
+    "url": "/static/media/alphero_horizontal.0838124f.png"
+  },
+  {
+    "revision": "0d793c227a889661704cb01b82c2d3a2",
+    "url": "/static/media/createcamp18_1.0d793c22.png"
+  },
+  {
+    "revision": "7b83dd14e41804f83a1e3cefbfad7d05",
+    "url": "/static/media/createcamp19_1.7b83dd14.png"
+  },
+  {
+    "revision": "decc9a7e4a6da24b776e1b143838646f",
+    "url": "/static/media/designday_poster.decc9a7e.png"
+  },
+  {
+    "revision": "e333deb635223df039e2217d02af0e2e",
+    "url": "/static/media/github.e333deb6.png"
+  },
+  {
+    "revision": "1e3e30f4ac308d34a8c3a869e083aad8",
+    "url": "/static/media/hsftf_1.1e3e30f4.png"
+  },
+  {
+    "revision": "c229763d546cb65b274577a8aaa1a889",
+    "url": "/static/media/ic_trophy.c229763d.png"
+  },
+  {
+    "revision": "a041c40baf11f12a8e5e33871c91d1da",
+    "url": "/static/media/ic_vuw.a041c40b.png"
+  },
+  {
+    "revision": "a8928fa98703fda931c3a33517a8287d",
+    "url": "/static/media/instagram.a8928fa9.png"
+  },
+  {
+    "revision": "72c0c96602325d3ed3eb75812e2e6ab9",
+    "url": "/static/media/mar_1.72c0c966.png"
+  },
+  {
+    "revision": "47a249b6619fd50e2f2d60b8e034da28",
+    "url": "/static/media/marlborough_lines.47a249b6.png"
+  },
+  {
+    "revision": "aad485144416112a172d82e32061fb82",
+    "url": "/static/media/me.aad48514.png"
+  },
+  {
+    "revision": "cd629d55e143d5971dc9abc27851800a",
+    "url": "/static/media/meetup.cd629d55.png"
+  },
+  {
+    "revision": "2da83260055b470af2ddee6030865633",
+    "url": "/static/media/sotdia_1.2da83260.png"
+  },
+  {
+    "revision": "5259864be52451c728d4e0e0fbea3f2f",
+    "url": "/static/media/soundcloud.5259864b.png"
+  },
+  {
+    "revision": "671ce43767b0f462dc4ac11d2a0d9f68",
+    "url": "/static/media/swwlg_1.671ce437.png"
+  },
+  {
+    "revision": "90721256d21a693b6d4a9ce613671518",
+    "url": "/static/media/td4g_1.90721256.png"
+  },
+  {
+    "revision": "a551facca500e46f4a10baeed38f7281",
+    "url": "/static/media/twitter.a551facc.png"
+  }
+]);
