c790a54fc7eafa8107412bd05f8eae45209a00e1
Liam Byrne <liam.jb1@gmail.com>
2021-06-13 13:30:59 +1200

    Version 1.1, build 16

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fe87b42d8b8639ea7f0506fe16b23c8c.js"
+  "/precache-manifest.0e84d52b0adbce9675874bc246cefaea.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
